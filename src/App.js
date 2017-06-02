cb83050361a365a8dfd1146f127f43444e67f87d
walwoodr <walwoodr@gmail.com>
2017-06-02 12:20:06 -0500

    Adjust CSS sources in files, cleanup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import Home from './components/Home';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Covfefe</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Home />
       </div>
     );
   }
 }
 
 export default App;
+
+
+  // <img src={logo} className="App-logo" alt="logo" />
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import './styles/index.css';
+import './styles/normalize.css';
+import './styles/skeleton.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
