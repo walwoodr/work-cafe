4c5abf452c79b65b1df56d4160fcdc997b630e34
walwoodr <walwoodr@gmail.com>
2017-06-05 11:55:26 -0500

    Add mock data and display

---
+++ src/components/CafeInput.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class CafeInput extends React.Component {
+  render(){
+    return (
+      <p> This is the input component</p>
+    )
+  }
+}
--- src/components/ShowCafe.js
+++ src/components/ShowCafe.js
@@ -1,17 +1,50 @@
 import React from 'react';
 import '../styles/showCafe.css';
 import CafeDetails from './CafeDetails';
+import CafeInput from './CafeInput';
+
+const cafeDetails = {
+  name: "Blue Moon",
+  zip: "55406",
+  address: "Lake Street and 39th Avenue South, Minenapolis MN",
+  hours: "7am-10pm Every day",
+  website: "bluemooncoffeecafe.com",
+  details: {
+    outlets: "some",
+    vibe: ["families", "networking", "people working"],
+    food: ["pastries", "vegan", "light meal", "full meal", "gluten free"],
+    genderNeutralRestrooms: true,
+    coffeeQuality: "medium",
+    teaQuality: "medium",
+    notes: [
+      {
+        user: "Cindy",
+        note: "I love this place!",
+        validated: true,
+      }, {
+        user: "Rachel",
+        note: "Nice place to work",
+        validated: false,
+      }, {
+        user: "Amanda",
+        note: "Queer friendly",
+        validated: true,
+      }
+    ]
+  }
+}
 
 export default class ShowCafe extends React.Component {
   render(){
     return (
       <div className="row">
-        <div className="three columns" />
+        <div className="three columns padding-div"><p>.</p></div>
         <div id="show_cafe" className="six columns">
-          Cafe show pane
-          <CafeDetails />
+          <p>Cafe show pane</p>
+          <CafeDetails cafeDetails={cafeDetails} />
+          <CafeInput />
         </div>
-        <div className="three columns" />
+        <div className="three columns padding-div"><p>.</p></div>
       </div>
     )
   }
