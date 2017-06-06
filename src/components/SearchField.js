99c130ff944b54cbd7c91f9cd8399a3960943ab4
walwoodr <walwoodr@gmail.com>
2017-06-06 18:13:34 -0500

    Refactor to a presentational component

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import '../styles/searchField.css';
 
-export default class SearchField extends React.Component {
-  handleChange(e){
+const SearchField = (props) => {
+  let handleChange = (e) => {
     e.preventDefault();
-    this.props.changeZipcode(e.target.value)
+    props.changeZipcode(e.target.value)
   };
 
-  handleSubmit(e){
-    e.preventDefault(); this.props.loadData(this.props.app_state.zipcode);
+  let handleSubmit = (e) => {
+    e.preventDefault(); props.loadData(props.app_state.zipcode);
   }
 
-  render(){
-      return (
-      <form onSubmit={(e) => this.handleSubmit(e)}>
-        <input id="search_field" type="text" placeholder="zip code" value={this.props.app_state.zipcode} onChange={(e) => this.handleChange(e)} />
-        <br />
-        <input id="search_button" className="button-primary" type="submit" value="Search" />
-      </form>
-    )
-  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input id="search_field" type="text" placeholder="zip code" value={props.app_state.zipcode} onChange={handleChange} />
+      <br />
+      <input id="search_button" className="button-primary" type="submit" value="Search" />
+    </form>
+  )
 }
+
+export default SearchField;
