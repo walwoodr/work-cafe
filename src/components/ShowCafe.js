ac85352a1a08daf4261b8e423e4b2be4953ea7e0
walwoodr <walwoodr@gmail.com>
2017-06-07 16:04:44 -0500

    Remove state from show cafe

--- src/components/ShowCafe.js
+++ src/components/ShowCafe.js
@@ -4,36 +4,28 @@ import { connect } from 'react-redux';
 
 // Components
 import CafeDetails from './CafeDetails';
-import CafeInput from './CafeInput';
 
 // Styles
 import '../styles/showCafe.css';
 
 export class ShowCafe extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      editing: false
-    }
-  }
-
-  handleClick(e){
-    e.preventDefault();
-    this.setState({
-      editing: true
-    })
-  }
-
   render(){
     return (
       <div className="row">
         <h2>{this.props.cafe.name}</h2>
-        <div className="three columns padding-div"><p>.</p></div>
-        <div id="show-cafe" className="six columns">
-          {this.state.editing ? <CafeInput cafeDetails={this.props.cafe} /> : <CafeDetails cafeDetails={this.props.cafe} />}
-          {this.state.editing ? '' : <button onClick={(e)=> this.handleClick(e)}>Share your knowledge</button>}
+
+        <div className="three columns padding-div">
+          <p>.</p>
         </div>
-        <div className="three columns padding-div"><p>.</p></div>
+
+        <div id="show-cafe" className="six columns">
+          <CafeDetails cafeDetails={this.props.cafe} />
+        </div>
+
+        <div className="three columns padding-div">
+          <p>.</p>
+        </div>
+
       </div>
     )
   }
