cc449dfa58bac9a837e0fac7a1ccf15bddb14c9d
walwoodr <walwoodr@gmail.com>
2017-06-07 16:29:49 -0500

    Add CoffeeTea detail component

--- src/components/CafeDetails.js
+++ src/components/CafeDetails.js
@@ -1,53 +1,43 @@
 import React from 'react';
-import coffee_beans from '../icons/coffee_beans.svg';
-import tea from '../icons/tea.svg';
-// import location from '../icons/location.svg';
-// import note from '../icons/note.svg';
 
 // Components
 import Outlets from './Outlets';
 import Bathroom from './Bathroom';
 import Food from './Food';
 import Vibe from './Vibe';
+import CoffeeTea from './CoffeeTea';
 
 // Styles
 import '../styles/cafeDetails.css'
 
 export default class CafeDetails extends React.Component{
 
-  // make this as a function that takes in the coffee and tea detail arguments?
-  coffeeTeaQuality = (quality) => {
-    if (quality !== null) {
-      var qualifier = quality.split(" ")[0]
-      return qualifier
-    } else {
-      return "unknown"
-    }
-  }
-
   render(){
     return (
       <div id="cafe-details">
-        <div className="category-detail-div">
-          <h3>Drinks</h3>
-          <div className="category-item-div">
-            <img src={coffee_beans} className="detail-icon" alt="coffee beans" />
-            <p> {this.coffeeTeaQuality(this.props.cafeDetails.coffeeQuality)} Coffee</p>
-          </div>
 
-          <div className="category-item-div">
-            <img src={tea} className="detail-icon" alt="tea" />
-            <p>{this.coffeeTeaQuality(this.props.cafeDetails.teaQuality)} Tea</p>
-          </div>
-        </div>
+        <CoffeeTea
+          coffee={this.props.cafeDetails.coffeeQuality}
+          tea={this.props.cafeDetails.teaQuality}
+        />
 
         <div id="amenities" className="multi-detail">
-          <Outlets outlets={this.props.cafeDetails.outlets} />
-          <Bathroom bathroom={this.props.cafeDetails.genderNeutralRestrooms} />
+          <Outlets
+            outlets={this.props.cafeDetails.outlets}
+          />
+          <Bathroom
+            bathroom={this.props.cafeDetails.genderNeutralRestrooms}
+          />
         </div>
 
-        <Food food={this.props.cafeDetails.food} />
-        <Vibe vibe={this.props.cafeDetails.vibe} />
+        <Food
+          food={this.props.cafeDetails.food}
+        />
+
+        <Vibe
+          vibe={this.props.cafeDetails.vibe}
+        />
+        
       </div>
     )
   }
---
+++ src/components/CoffeeTea.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import coffee from '../icons/coffee_beans.svg';
+import tea from '../icons/tea.svg';
+
+export default (props) => {
+
+  var divClasses;
+  if (props.tea === null || props.coffee === null) {
+    divClasses = "edit-detail category-detail-div"
+  } else {
+    divClasses = "category-detail-div"
+  }
+
+  var words = (drinkProp, type) => {
+    if (drinkProp) {
+      return (<p>{drinkProp}</p>)
+    } else {
+      return (<div><p>Unknown {type}</p><p className="note">click to edit</p></div>)
+    }
+  }
+
+  return (
+    <div className={divClasses}>
+      <h3>Drinks</h3>
+
+      <div className="category-item-div">
+        <img src={coffee} className="detail-icon" alt="coffee" />
+        <p>{words(props.coffee, "Coffee")}</p>
+      </div>
+
+      <div className="category-item-div">
+      <img src={tea} className="detail-icon" alt="tea" />
+      <p>{words(props.tea, "Tea")}</p>
+      </div>
+
+    </div>
+  )
+}
