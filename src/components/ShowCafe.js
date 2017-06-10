6ce16b44f8b4680997ca5ba4abd56864ca0f0ff3
walwoodr <walwoodr@gmail.com>
2017-06-10 12:31:16 -0500

    Remove default cafe and add error handling

--- src/components/ShowCafe.js
+++ src/components/ShowCafe.js
@@ -18,8 +18,10 @@ import peaceCoffee from '../defaultcafe.js'
 
 export class ShowCafe extends React.Component {
   componentWillMount(){
-    if (this.props.cafe === peaceCoffee ) {
-      this.props.history.push("/cafes/1");
+    console.log("this.props.cafe")
+    console.log(this.props.cafe)
+    if (this.props.cafe === null ) {
+      this.props.history.push("/search");
     }
   }
 
@@ -30,30 +32,34 @@ export class ShowCafe extends React.Component {
   }
 
   render(){
-    return (
-      <div className="row">
-        <h2>{this.props.cafe.name}</h2>
+    if (this.props.cafe !== null){
+      return (
+        <div className="row">
+          <h2>{this.props.cafe.name}</h2>
 
-        <div className="two columns padding-div">
-          <p>.</p>
+          <div className="two columns padding-div">
+            <p>.</p>
+          </div>
+
+          <div id="show-cafe" className="eight columns">
+            <CafeDetails
+              cafeDetails={this.props.cafe}
+              editingDetail={this.props.editingDetail}
+              editing={this.props.app_state.editing}
+              modifyCafe={this.props.modifyCafe}
+              clearEdit={this.props.clearEdit}
+             />
+          </div>
+
+          <div className="two columns padding-div">
+            <p>.</p>
+          </div>
+
         </div>
-
-        <div id="show-cafe" className="eight columns">
-          <CafeDetails
-            cafeDetails={this.props.cafe}
-            editingDetail={this.props.editingDetail}
-            editing={this.props.app_state.editing}
-            modifyCafe={this.props.modifyCafe}
-            clearEdit={this.props.clearEdit}
-           />
-        </div>
-
-        <div className="two columns padding-div">
-          <p>.</p>
-        </div>
-
-      </div>
-    )
+      )
+    } else {
+      return <div>Please search for your location to view cafes.</div>
+    }
   }
 }
 
@@ -63,7 +69,7 @@ function mapStateToProps(state, ownProps){
   if (cafe) {
     return {cafe: cafe, app_state: state.app_state}
   } else {
-    return {cafe: peaceCoffee, app_state: state.app_state}
+    return {cafe: null, app_state: state.app_state}
   }
 }
 
