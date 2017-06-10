8b79eac85f46e980a13a07415d2a5e68d870f92c
walwoodr <walwoodr@gmail.com>
2017-06-10 13:23:35 -0500

    Add nav styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/nav.css';
 
 var NavBar = (props) => {
   return (
-    <ul>
-      <li onClick={props.viewingComponent.bind(null, "search")}><Link to={'/search'}>Search</Link></li>
-      <li onClick={props.viewingComponent.bind(null, "cafeIndex")}><Link to={'/cafes'}>Cafes</Link></li>
+    <ul id="nav-bar">
+      <Link to={'/search'}>
+        <li
+          className={props.nowViewing === "search" ? "nav-link current" : "nav-link"}
+          id="search-link"
+          onClick={props.viewingComponent.bind(null, "search")}>
+            Search
+        </li>
+      </Link>
+      <Link to={'/cafes'}>
+        <li
+          className={props.nowViewing === "cafeIndex" ? "nav-link current" : "nav-link"}
+          id="cafe-index-link"
+          onClick={props.viewingComponent.bind(null, "cafeIndex")}>
+            Cafes
+        </li>
+      </Link>
     </ul>
   )
 }
