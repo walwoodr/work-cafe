1a057b9269f5468440fd08e538d938c13ecf65e6
walwoodr <walwoodr@gmail.com>
2017-06-15 09:52:37 -0500

    Add tooltip to restroom input

--- src/components/Bathroom.js
+++ src/components/Bathroom.js
@@ -1,23 +1,39 @@
+//Libs
 import React from 'react';
+import ReactTooltip from 'react-tooltip';
+
+// Icons
 import toilet from '../icons/toilet.svg';
 import gendered_toilet from '../icons/gendered_toilet.svg';
 
 export default (props) => {
+  var bathroomText = {
+    genderNeutral: "At least one gender neutral or single stall restroom",
+    genderSegregated: "Only gender segregated restrooms"
+  }
   var bathroom;
   if (props.bathroom) {
-    bathroom = <div className="detail-div" id="gender-neutral">
+    bathroom = <div className="detail-div" id="gender-neutral" data-tip data-for="genderNeutral">
 
       <img src={toilet} className="detail-icon" alt="Gender Neutral Restroom" />
 
       <p>Gender Neutral Restroom</p>
 
+      <ReactTooltip id="genderNeutral" place="right" type="dark" effect="solid" className="toolTipTheme">
+        <span>{bathroomText.genderNeutral}</span>
+      </ReactTooltip>
+
     </div>
   } else if (props.bathroom === false) {
-    bathroom = <div className="detail-div" id="gender-neutral">
+    bathroom = <div className="detail-div" id="gender-neutral" data-tip datta-for="genderSegregated">
 
       <img src={gendered_toilet} className="detail-icon" alt="Gender Segregated Restroom" />
 
       <p>Gender Segregated Restrooms</p>
+
+      <ReactTooltip id="genderSegregated" place="right" type="dark" effect="solid" className="toolTipTheme">
+        <span>{bathroomText.genderSegregated}</span>
+      </ReactTooltip>
 
     </div>
   } else if (props.editing) {
@@ -28,15 +44,23 @@ export default (props) => {
     bathroom = <div className="edit-detail detail-div" id="gender-neutral">
 
       <p>Are there gender neutral restrooms in this cafe?</p>
-      <div className="select-option">
+      <div className="select-option" data-tip data-for="genderNeutral">
         <button onClick={sendAndClearEdit.bind(null, true)}>Yes!</button>
-        <p className="detail-note">At least one gender neutral or single stall restroom</p>
       </div>
 
-      <div className="select-option">
+      <ReactTooltip id="genderNeutral" place="bottom" type="dark" effect="solid" className="toolTipTheme">
+        <span>{bathroomText.genderNeutral}</span>
+      </ReactTooltip>
+
+
+      <div className="select-option" data-tip data-for="genderSegregated">
         <button onClick={sendAndClearEdit.bind(null, false)}>No.</button>
-        <p className="detail-note">Only gender segregated restrooms</p>
       </div>
+
+      <ReactTooltip id="genderSegregated" place="bottom" type="dark" effect="solid" className="toolTipTheme">
+        <span>{bathroomText.genderSegregated}</span>
+      </ReactTooltip>
+
 
       <p className="note" onClick={(e) => props.handleBlur(e)}>click to close</p>
 
